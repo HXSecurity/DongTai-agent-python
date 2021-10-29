664cb240908f1e5e0915e23b9edc14478b14def9
lostsnow <lostsnow@gmail.com>
2021-10-29 18:49:12 +0800

    add run id to vul test script

--- dongtai_agent_python/tests/vul-test.sh
+++ dongtai_agent_python/tests/vul-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 HOST=$1
-
+RUN_ID=$2
 if [ -z ${HOST} ]; then
   echo -e "api host required!"
   echo -e "Usage: $( basename $0 ) <api host>"
@@ -9,36 +9,36 @@ if [ -z ${HOST} ]; then
 fi
 
 # exec-command
-curl "${HOST}/api/demo/exec_post_popen" -X POST --data-raw 'code=ls'
+curl "${HOST}/api/demo/exec_post_popen?_r=${RUN_ID}" -X POST --data-raw 'code=ls'
 echo
-curl "${HOST}/api/demo/exec_post_subprocess" -X POST --data-raw 'cmd=cat&name=%2Fetc%2Fpasswd'
+curl "${HOST}/api/demo/exec_post_subprocess?_r=${RUN_ID}" -X POST --data-raw 'cmd=cat&name=%2Fetc%2Fpasswd'
 echo
-curl "${HOST}/api/demo/cmd_exec" -X POST --data-raw 'cmd=whoami'
+curl "${HOST}/api/demo/cmd_exec?_r=${RUN_ID}" -X POST --data-raw 'cmd=whoami'
 echo
-curl "${HOST}/api/demo/exec_post_e" -X POST --data-raw 'code=whoami'
+curl "${HOST}/api/demo/exec_post_e?_r=${RUN_ID}" -X POST --data-raw 'code=whoami'
 echo
 # exec-code
-curl "${HOST}/api/demo/eval_post_e" -X POST --data-raw 'code=__import__%28%27os%27%29.system%28%27whoami%27%29'
+curl "${HOST}/api/demo/eval_post_e?_r=${RUN_ID}" -X POST --data-raw 'code=__import__%28%27os%27%29.system%28%27whoami%27%29'
 echo
-curl "${HOST}/api/demo/yaml_post_e" -X POST --data-raw 'code=whoami'
+curl "${HOST}/api/demo/yaml_post_e?_r=${RUN_ID}" -X POST --data-raw 'code=whoami'
 echo
 # path-traversal
-curl "${HOST}/api/demo/get_open?name=Data**"
+curl "${HOST}/api/demo/get_open?name=Data**&_r=${RUN_ID}"
 echo
-curl "${HOST}/api/demo/post_open" -X POST --data-raw 'name=.%2Ffile%2Fdata.json'
+curl "${HOST}/api/demo/post_open?_r=${RUN_ID}" -X POST --data-raw 'name=.%2Ffile%2Fdata.json'
 echo
 # sql-injection
-curl "${HOST}/api/demo/postgresql_post_many" -X POST --data-raw 'id=100&name=song&phone1=13300000000'
+curl "${HOST}/api/demo/postgresql_post_many?_r=${RUN_ID}" -X POST --data-raw 'id=100&name=song&phone1=13300000000'
 echo
-curl "${HOST}/api/demo/postgresql_post_excute" -X POST --data-raw 'name=song'
+curl "${HOST}/api/demo/postgresql_post_excute?_r=${RUN_ID}" -X POST --data-raw 'name=song'
 echo
-curl "${HOST}/api/demo/mysql_post_many" -X POST --data-raw 'name=song&phone1=13300000000'
+curl "${HOST}/api/demo/mysql_post_many?_r=${RUN_ID}" -X POST --data-raw 'name=song&phone1=13300000000'
 echo
-curl "${HOST}/api/demo/mysql_post_exec" -X POST --data-raw 'name=song'
+curl "${HOST}/api/demo/mysql_post_exec?_r=${RUN_ID}" -X POST --data-raw 'name=song'
 echo
-curl "${HOST}/api/demo/sqlite3_post_executemany_sql" -X POST --data-raw 'phone1=13300000000'
+curl "${HOST}/api/demo/sqlite3_post_executemany_sql?_r=${RUN_ID}" -X POST --data-raw 'phone1=13300000000'
 echo
-curl "${HOST}/api/demo/sqlite3_post_executescript" -X POST --data-raw 'name=song&phone1=13300000000'
+curl "${HOST}/api/demo/sqlite3_post_executescript?_r=${RUN_ID}" -X POST --data-raw 'name=song&phone1=13300000000'
 echo
-curl "${HOST}/api/demo/sqlite3_post" -X POST --data-raw 'name=song'
+curl "${HOST}/api/demo/sqlite3_post?_r=${RUN_ID}" -X POST --data-raw 'name=song'
 echo
