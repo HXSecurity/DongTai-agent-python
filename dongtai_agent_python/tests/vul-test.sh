c892a174bd5ed629af5beef6d997359296c1ad45
lostsnow <lostsnow@gmail.com>
2021-11-05 13:33:34 +0800

    fixes xxe payload

--- dongtai_agent_python/tests/vul-test.sh
+++ dongtai_agent_python/tests/vul-test.sh
@@ -104,16 +104,7 @@ api_get "demo/xss_template" "content=alert"
 api_get "demo/xss_template_string" "content=alert"
 
 headline "xxe"
-api_post "demo/xxe_login" '<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE Anything [
-<!ENTITY xxe SYSTEM "file:///etc/passwd">
-]>
-<user>
- <username>&xxe;</username>
- <password>
-   yzx
- </password>
-</user>'
+api_post "demo/xxe_login" '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE Anything [<!ENTITY xxe SYSTEM "file:///etc/passwd">]><user><username>&xxe;</username><password>yzx</password></user>'
 
 headline "ssrf"
 api_get "demo/urllib_ssrf" "url=https://www.huoxian.cn/"
