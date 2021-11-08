8cd9a36cd9bfb7ce3752e25ee99edfbbdf0c4671
lostsnow <lostsnow@gmail.com>
2021-11-08 15:31:08 +0800

    fixes sql injection sink arguments processing

--- dongtai_agent_python/assess/deal_data.py
+++ dongtai_agent_python/assess/deal_data.py
@@ -7,12 +7,8 @@ from dongtai_agent_python.common.content_tracert import method_pool_data, dt_tra
 
 
 def wrapData(result, origin_cls, _fcn, signature=None, node_type=None, comeData=None, comeKwArgs=None):
-    if node_type != utils.NODE_TYPE_SINK and utils.is_empty(result):
+    if not filter_result(result, node_type):
         return result
-
-    if node_type == utils.NODE_TYPE_SOURCE:
-        if utils.is_not_allowed_type(result):
-            return result
 
     dt_open_pool = _global_dt_dict.get("dt_open_pool")
     if not dt_open_pool:
@@ -24,13 +20,7 @@ def wrapData(result, origin_cls, _fcn, signature=None, node_type=None, comeData=
     if dt_data_args is None:
         return result
 
-    invokeArgs = []
-    if comeData is not None:
-        for v in comeData:
-            origin.list_append(invokeArgs, v)
-    if comeKwArgs is not None:
-        for k in comeKwArgs:
-            origin.list_append(invokeArgs, comeKwArgs[k])
+    invokeArgs = processing_invoke_args(signature, comeData, comeKwArgs)
 
     # 获取source点
     taint_in = []
@@ -64,3 +54,54 @@ def wrapData(result, origin_cls, _fcn, signature=None, node_type=None, comeData=
     dt_global_var.dt_set_value("dt_open_pool", True)
 
     return result
+
+
+def filter_result(result, node_type=None):
+    if node_type != utils.NODE_TYPE_SINK and utils.is_empty(result):
+        return False
+
+    if node_type == utils.NODE_TYPE_SOURCE:
+        if utils.is_not_allowed_type(result):
+            return False
+
+    return True
+
+
+def processing_invoke_args(signature=None, comeData=None, comeKwArgs=None):
+    sink_args = {
+        'sqlite3.Cursor.execute': {'args': [0]},
+        'sqlite3.Cursor.executemany': {'args': [0]},
+        'sqlite3.Cursor.executescript': {'args': [0]},
+        'psycopg2._psycopg.cursor.execute': {'args': [0], 'kwargs': ['query']},
+        'psycopg2._psycopg.cursor.executemany': {'args': [0], 'kwargs': ['query']},
+        'MySQLdb.cursors.BaseCursor.execute': {'args': [0], 'kwargs': ['query']},
+        'MySQLdb.cursors.BaseCursor.executemany': {'args': [0], 'kwargs': ['query']},
+        'pymysql.cursors.Cursor.execute': {'args': [0], 'kwargs': ['query']},
+        'pymysql.cursors.Cursor.executemany': {'args': [0], 'kwargs': ['query']},
+        'mysql.connector.cursor.CursorBase.execute': {'args': [0], 'kwargs': ['operation']},
+        'mysql.connector.cursor.CursorBase.executemany': {'args': [0], 'kwargs': ['operation']},
+    }
+
+    invokeArgs = []
+    if signature not in sink_args:
+        if comeData is not None:
+            for v in comeData:
+                origin.list_append(invokeArgs, v)
+        if comeKwArgs is not None:
+            for k in comeKwArgs:
+                origin.list_append(invokeArgs, comeKwArgs[k])
+
+        return invokeArgs
+
+    if comeData and len(comeData) > 0 and 'args' in sink_args[signature]:
+        args_size = len(comeData)
+        for arg in sink_args[signature]:
+            if args_size > arg:
+                origin.list_append(invokeArgs, comeData[arg])
+
+    if comeKwArgs and len(comeKwArgs) > 0 and 'kwargs' in sink_args[signature]:
+        for key in sink_args[signature]:
+            if key in comeKwArgs:
+                origin.list_append(invokeArgs, comeKwArgs[key])
+
+    return invokeArgs
