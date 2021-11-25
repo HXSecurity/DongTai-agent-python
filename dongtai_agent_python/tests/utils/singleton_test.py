825986400a941cf97f69d221e6abb3874e5aff2c
lostsnow <lostsnow@gmail.com>
2021-11-25 11:39:18 +0800

    add singleton

---
+++ dongtai_agent_python/tests/utils/singleton_test.py
@@ -0,0 +1,45 @@
+import threading
+import unittest
+
+from dongtai_agent_python.utils.singleton import Singleton
+
+
+class TestSingleton(unittest.TestCase):
+    def test_singleton(self):
+        class Test(Singleton):
+            def init(self, seq=0):
+                self.seq = seq
+
+            def incr(self):
+                self.seq = self.seq + 1
+
+        s1 = Test(1)
+        s2 = Test(2)
+        self.assertEqual(s1, s2)
+        self.assertEqual(1, s1.seq)
+
+        s1.incr()
+        s2.incr()
+        self.assertEqual(3, s1.seq)
+
+    def test_multithreading(self):
+        class Test(Singleton):
+            def init(self, seq=0):
+                self.seq = seq
+
+            def incr(self):
+                self.seq = self.seq + 1
+
+        def test_incr():
+            s = Test()
+            s.incr()
+
+        threads = []
+        thread_num = 5
+        for i in range(thread_num):
+            t = threading.Thread(target=test_incr)
+            threads.append(t)
+            t.start()
+
+        s = Test()
+        self.assertEqual(thread_num, s.seq)
---
+++ dongtai_agent_python/utils/singleton.py
@@ -0,0 +1,12 @@
+# @see: https://www.python.org/download/releases/2.2/descrintro/#__new__
+class Singleton(object):
+    def __new__(cls, *args, **kwargs):
+        it = cls.__dict__.get("__it__")
+        if it is not None:
+            return it
+        cls.__it__ = it = object.__new__(cls)
+        it.init(*args, **kwargs)
+        return it
+
+    def init(self, *args, **kwargs):
+        pass
