87a3b9608c4e91e19bfb07dfe3462af6daa162b2
lostsnow <lostsnow@gmail.com>
2021-12-07 16:06:44 +0800

    c api hook callback

--- dongtai_agent_python/assess/c_api_hook.py
+++ dongtai_agent_python/assess/c_api_hook.py
@@ -1,6 +1,3 @@
-import ctypes
-import os
-
 from dongtai_agent_python.utils import scope
 
 
@@ -12,17 +9,19 @@ def callback_unicode_fstring(target, self, result, *args, **kwargs):
 
 @scope.scope(scope.SCOPE_AGENT)
 def callback_bytes_cformat(target, self, result, *args, **kwargs):
-    # @TODO: processing taint data
-    pass
+    return callback_cformat(target, self, result, *args, **kwargs)
 
 
 @scope.scope(scope.SCOPE_AGENT)
 def callback_bytearray_cformat(target, self, result, *args, **kwargs):
-    # @TODO: processing taint data
-    pass
+    return callback_cformat(target, self, result, *args, **kwargs)
 
 
 @scope.scope(scope.SCOPE_AGENT)
 def callback_unicode_cformat(target, self, result, *args, **kwargs):
+    return callback_cformat(target, self, result, *args, **kwargs)
+
+
+def callback_cformat(target, self, result, *args, **kwargs):
     # @TODO: processing taint data
     pass
