aca5bd38ebdf2d4423d61dcb27aa1a7d4ae96e1d
lostsnow <lostsnow@gmail.com>
2021-12-10 17:09:01 +0800

    add str/bytes/bytearray cast patches

--- dongtai_agent_python/assess/c_api_hook.py
+++ dongtai_agent_python/assess/c_api_hook.py
@@ -3,29 +3,36 @@ from dongtai_agent_python.setting import const
 from dongtai_agent_python.utils import scope
 
 
-def callback_unicode_fstring(target, self, result, *args, **kwargs):
+def callback_propagator(target, origin_cls, origin_func, signature, args=None, kwargs=None):
     if scope.in_scope(scope.SCOPE_AGENT):
         return
-    wrap_data(target, 'str', 'fstring', signature='builtins.str.fstring',
+    wrap_data(target, origin_cls, origin_func, signature=signature,
               node_type=const.NODE_TYPE_PROPAGATOR, come_args=args, come_kwargs=kwargs)
+
+
+def callback_unicode_fstring(target, self, result, *args, **kwargs):
+    callback_propagator(target, 'str', 'fstring', signature='builtins.str.fstring', args=args, kwargs=kwargs)
 
 
 def callback_bytes_cformat(target, self, result, *args, **kwargs):
-    if scope.in_scope(scope.SCOPE_AGENT):
-        return
-    wrap_data(target, 'bytes', 'cformat', signature='builtins.bytes.cformat',
-              node_type=const.NODE_TYPE_PROPAGATOR, come_args=args, come_kwargs=kwargs)
+    callback_propagator(target, 'bytes', 'cformat', signature='builtins.bytes.cformat', args=args, kwargs=kwargs)
 
 
 def callback_bytearray_cformat(target, self, result, *args, **kwargs):
-    if scope.in_scope(scope.SCOPE_AGENT):
-        return
-    wrap_data(target, 'bytearray', 'cformat', signature='builtins.bytearray.cformat',
-              node_type=const.NODE_TYPE_PROPAGATOR, come_args=args, come_kwargs=kwargs)
+    callback_propagator(target, 'bytearray', 'cformat', signature='builtins.bytearray.cformat', args=args, kwargs=kwargs)
 
 
 def callback_unicode_cformat(target, self, result, *args, **kwargs):
-    if scope.in_scope(scope.SCOPE_AGENT):
-        return
-    wrap_data(target, 'str', 'cformat', signature='builtins.str.cformat',
-              node_type=const.NODE_TYPE_PROPAGATOR, come_args=args, come_kwargs=kwargs)
+    callback_propagator(target, 'str', 'cformat', signature='builtins.str.cformat', args=args, kwargs=kwargs)
+
+
+def callback_bytes_cast(target, self, result, *args, **kwargs):
+    callback_propagator(target, 'bytes', '__new__', signature='builtins.bytes.__new__', args=args, kwargs=kwargs)
+
+
+def callback_bytearray_cast(target, self, result, *args, **kwargs):
+    callback_propagator(target, 'bytearray', '__init__', signature='builtins.bytearray.__init__', args=args, kwargs=kwargs)
+
+
+def callback_unicode_cast(target, self, result, *args, **kwargs):
+    callback_propagator(target, 'str', '__new__', signature='builtins.str.__new__', args=args, kwargs=kwargs)
---
+++ dongtai_agent_python/assess_ext/patch/cast.c
@@ -0,0 +1,66 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include <funchook.h>
+#include <logger.h>
+#include <utils.h>
+#include <patch.h>
+
+
+#define IS_TRACKABLE(X) \
+    (PyUnicode_Check((X)) || PyBytes_Check((X)) || PyByteArray_Check((X)))
+
+newfunc bytes_cast_origin;
+initproc bytearray_cast_origin;
+newfunc unicode_cast_origin;
+
+PyObject *bytes_cast_new(PyTypeObject *type, PyObject *args, PyObject *kwargs) {
+    PyObject *result = bytes_cast_origin(type, args, kwargs);
+
+    if (result == NULL) {
+        return result;
+    }
+
+    patch_string_callback("callback_bytes_cast", NULL, result, args, kwargs);
+
+    return result;
+}
+
+int bytearray_cast_new(PyObject *self, PyObject *args, PyObject *kwargs) {
+    int result = bytearray_cast_origin(self, args, kwargs);
+
+    if (result == -1) {
+        return result;
+    }
+
+    patch_string_callback("callback_bytearray_cast", NULL, self, args, kwargs);
+
+    return result;
+}
+
+PyObject *unicode_cast_new(PyTypeObject *type, PyObject *args, PyObject *kwargs) {
+    PyObject *result = unicode_cast_origin(type, args, kwargs);
+
+    if (result == NULL) {
+        return result;
+    }
+
+    patch_string_callback("callback_unicode_cast", NULL, result, args, kwargs);
+
+    return result;
+}
+
+int apply_cast_patch(funchook_t *funchook) {
+    bytes_cast_origin = (void *)PyBytes_Type.tp_new;
+    funchook_prepare_wrapper(funchook, &bytes_cast_origin, bytes_cast_new);
+
+    bytearray_cast_origin = (void *)PyByteArray_Type.tp_init;
+    funchook_prepare_wrapper(funchook, &bytearray_cast_origin, bytearray_cast_new);
+
+    unicode_cast_origin = (void *)PyUnicode_Type.tp_new;
+    funchook_prepare_wrapper(funchook, &unicode_cast_origin, unicode_cast_new);
+
+    log_debug("------c_patch------------------ cast");
+
+    return 0;
+}
